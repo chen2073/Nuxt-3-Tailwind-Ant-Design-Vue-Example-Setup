e67a68454429ab0d790ce69e9eaab431d82dec73
James <29440870+chen2073@users.noreply.github.com>
2023-01-23 12:41:30 -0800

    layout  added

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+    <a-layout :style="{ height: '100vh' }">
+      <a-layout-sider width="280" style="background: #2C55B5">
+        <div>
+          <h3>
+            My Organization
+          </h3>
+          <a-menu mode="inline">
+            <a-menu-item key="1">
+              <span>People</span>
+            </a-menu-item>
+          </a-menu>
+        </div>
+      </a-layout-sider>
+      <a-layout style="padding: 24px 24px 24px">
+        <a-layout-content
+          :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '500px'}"
+        >
+          <slot />
+        </a-layout-content>
+      </a-layout>
+    </a-layout>
+</template>
---
+++ pages/counter.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+if (process.server) {
+  console.log('server')
+} else {
+  console.log('client')
+}
+
+const color = useColor()
+const counter = useCounter()
+</script>
+
+<template>
+  <div>
+    <h2>Current color: {{ color }}</h2>
+    <input class="text-purple"/>
+    <div>
+      Count: {{ counter }}
+      <a-button type="primary" @click="counter++">
+        +
+      </a-button>
+      <a-button type="primary" @click="counter--">
+        -
+      </a-button>
+    </div>
+  </div>
+</template>
